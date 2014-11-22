9073a85338f7f75a301c6ebbba4d9f8b9a965612
od3n <mr.od3n@gmail.com>
2014-11-22 08:55:37 +0000

    Update bookmark. Contains tests.

--- test/controllers/bookmarks_controller_test.rb
+++ test/controllers/bookmarks_controller_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class BookmarksControllerTest < ActionController::TestCase
-  test "return ok status if bookmark is created" do
+  test "returns ok status if bookmark is created" do
   	post :create, bookmark: { title: "Tuts+", url: "http://tutsplus.com" }
 
   	assert_response 201
   end
 
-  test "return not ok status if bookmark is not created" do
+  test "returns not ok status if bookmark is not created" do
   	post :create, bookmark: {}
 
   	assert_response 422
+  end
+
+  test "returns ok if a record is updated" do
+  	bookmark = Bookmark.create! ({ title: "Tuts+", url: "http://tutsplus.com"})
+
+  	put :update, id: bookmark.id, bookmark: { title: "Net Tuts" }
+
+  	assert_response 204 # No Content
   end
 end
